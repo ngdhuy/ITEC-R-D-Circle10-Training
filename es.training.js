c34f668eeda1764737ba47b9f92b3934d26ffc4a
Ai Jin <1659017@itec.hcmus.edu.vn>
2020-01-07 16:52:57 +0700

    Ai Jin 7/1

---
+++ es.training.js
@@ -0,0 +1,37 @@
+class Animal{
+    constructor(type = 'animal'){
+        this.type = type
+   }
+   get type(){
+       return this._type
+   }
+
+   set type(val){
+       this._type = val.toUpperCase()
+   }
+
+   makeSound(){
+       console.log('Making animal sound')
+   }
+}
+
+let a = new Animal()
+console.log(a.type)
+
+class Cat extends Animal 
+{
+    constructor(){
+        super('cat')
+    }
+
+    makeSound(){
+        super.makeSound()
+        console.log('Meow!')
+    }
+}
+
+let b = new Cat()
+console.log(b.type)
+
+console.log(a)
+console.log(b)
